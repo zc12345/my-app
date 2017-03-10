4c340e3b4f613995b15167523d79c99036f1d20f
zc12345 <18292885866@163.com>
2017-03-11 00:27:24 +0800

    增加了一部分组件的声明，写了今天的工作总结

--- src/MyRoute.js
+++ src/MyRoute.js
@@ -3,9 +3,10 @@ import React from 'react';
 import {Router, Route, browserHistory, hashHistory} from 'react-router';
 
 import Main from './components/Main';
-import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
+import Home from './components/Home';
+import User from './components/User';
 import Manage from './components/Manage';
 import HomeworkList from './components/HomeworkList';
 import Interview from './components/IntervieweeList';
@@ -17,7 +18,9 @@ export default class MyRoute extends React.Component {
                 <Route path='/' component={Main}>
                     <Route path='/login' component={Login}/>
                     <Route path='/register' component={Register}/>
-                    <Route path='/home' component={Home}/>
+                    <Route path='/home' component={Home}>
+                        <Route path='/user' component={User}></Route>
+                    </Route>
                     <Route path='/manage' component={Manage}>
                         <Route path='/interview' component={Interview}/>
                         <Route path='/homeworkList' component={HomeworkList}/>            
--- src/components/Home.js
+++ src/components/Home.js
@@ -20,9 +20,9 @@ export default class Home extends React.Component {
             style={{ height: '100%' }}
           >
             <SubMenu key="sub1" title={<span><Icon type="user" />个人中心</span>}>
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="1"><Link to='/user'>修改信息</Link></Menu.Item>
+              <Menu.Item key="2">提交作业</Menu.Item>
+              <Menu.Item key="3">文件下载</Menu.Item>
               <Menu.Item key="4">option4</Menu.Item>
             </SubMenu>
             <SubMenu key="sub2" title={<span><Icon type="laptop" />社团文化</span>}>
@@ -32,15 +32,15 @@ export default class Home extends React.Component {
               <Menu.Item key="8">option8</Menu.Item>
             </SubMenu>
             <SubMenu key="sub3" title={<span><Icon type="notification" />最新通知</span>}>
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="9">例会通知</Menu.Item>
+              <Menu.Item key="10">作业通知</Menu.Item>
               <Menu.Item key="11">option11</Menu.Item>
               <Menu.Item key="12">option12</Menu.Item>
             </SubMenu>
           </Menu>
         </Sider>
         <Content style={{ padding: '0 24px', minHeight: 280 }}>
-          Content
+          {this.props.children}
         </Content>
       </Layout>
       );
